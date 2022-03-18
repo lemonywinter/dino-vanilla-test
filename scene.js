29251594c73dc687d1cb96a6792b06783be90512
Erica H <lemonostril@gmail.com>
2022-03-18 15:36:35 -0400

    Deleted unnecessary files and reorganized structure

--- scene.js
+++
@@ -1,9 +0,0 @@
-let scenes = {
-  title: "Scene 1",
-  text: "This is the first scene. Observe!",
-  sprite: "greenMan",
-  spritePositionX: 0,
-  spritePositionY: 0,
-  bg: "bg",
-  audio: "path",
-};
